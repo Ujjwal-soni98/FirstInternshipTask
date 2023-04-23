acf356fc1028373d956343dc4c794f87b03cc1e8
Ujjwal-soni-98 <ujjwal.soniji@gmail.com>
2023-04-23 19:22:31 +0530

    initial COMMIT

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      colors:{
+        
+      }
+    },
+    fontFamily:{
+      sans:['Noto Serif','serif'],
+      heading1:['Playfair Display SC','serif'],
+    }
+  },
+  plugins: [],
+}
+
